d931443da376f9a7a17de241c66a95b416fde169
Shige0352 <shigeaki52@gmail.com>
2020-05-11 21:53:16 +0900

    first commit

---
+++ src/main/java/com/tokokun/api/TokokunApplication.java
@@ -0,0 +1,13 @@
+package com.tokokun.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokokunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokokunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tokokun/TokokunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tokokun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokokunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
